4d6a1df53fd0da08b02e9a2dfcc9199bdcb064f1
sounak saha <sounaksaha100@gmail.com>
2023-02-21 20:34:12 +0530

    functional class declared

---
+++ app/src/main/java/com/codingwithme/authentication/signInActivity.kt
@@ -0,0 +1,12 @@
+package com.codingwithme.authentication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class signInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+    }
+}
---
+++ app/src/main/java/com/codingwithme/authentication/signUpActivity.kt
@@ -0,0 +1,12 @@
+package com.codingwithme.authentication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class signUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+    }
+}
