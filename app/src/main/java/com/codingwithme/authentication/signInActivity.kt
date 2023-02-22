6f1bd7135d358d6612f87ccfe5f48c30889761ad
sounak saha <sounaksaha100@gmail.com>
2023-02-22 13:08:42 +0530

    Features updated

--- app/src/main/java/com/codingwithme/authentication/signInActivity.kt
+++ app/src/main/java/com/codingwithme/authentication/signInActivity.kt
@@ -1,12 +1,47 @@
 package com.codingwithme.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.codingwithme.authentication.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class signInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        setContentView(binding.root)
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, signUpActivity::class.java)
+            startActivity(intent)
+        }
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+
+            if (email != null && pass != null) {
+
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            } else {
+                Toast.makeText(this, "Email address can't be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
--- app/src/main/java/com/codingwithme/authentication/signUpActivity.kt
+++ app/src/main/java/com/codingwithme/authentication/signUpActivity.kt
@@ -1,12 +1,63 @@
 package com.codingwithme.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
+import com.codingwithme.authentication.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
+private val Any.java: Class<*>?
+    get() {
+        return null
+    }
 
 class signUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        //FIrebase Intialized
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, signInActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+            val confirmPass = binding.confirmPassEt.text.toString()
+
+            if (email != null && pass != null && confirmPass != null) {
+
+                if (pass == confirmPass) {
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this,signInActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Please type the Password correctly", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            } else {
+                Toast.makeText(this, "Email address can't be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
